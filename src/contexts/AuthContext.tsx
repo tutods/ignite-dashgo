cf63dd0af5e4d4ea5bfa2ee5ba919774655bdd0c
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-10-16 07:47:41 +0100

    feat: save token and refresh token on cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,70 +1,81 @@
-import { createContext, ReactNode, useState } from "react";
-import { SignInFormData } from "shared/data/schemas/SignIn.schema";
-import { fetchData } from "shared/services/api";
-import { AuthResponse } from "shared/@types/AuthResponse";
-import { useRouter } from "next/router";
+import { createContext, ReactNode, useState } from 'react';
+import { SignInFormData } from 'shared/data/schemas/SignIn.schema';
+import { fetchData } from 'shared/services/api';
+import { AuthResponse } from 'shared/@types/AuthResponse';
+import { useRouter } from 'next/router';
+import { setCookie } from 'nookies';
 
 type UserData = {
-  email: string;
-  roles: string[];
-  permissions: string[];
+	email: string;
+	roles: string[];
+	permissions: string[];
 };
 
 type AuthData = {
-  token: string;
-  refreshToken: string;
+	token: string;
+	refreshToken: string;
 };
 
 type AuthContextData = {
-  user: UserData;
-  isAuthenticated: boolean;
-  signIn: (credentials: SignInFormData) => Promise<void>;
+	user: UserData;
+	isAuthenticated: boolean;
+	signIn: (credentials: SignInFormData) => Promise<void>;
 };
 
 type AuthProviderProps = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 const AuthContext = createContext({} as AuthContextData);
 
 const AuthContextProvider = ({ children }: AuthProviderProps) => {
-  const [user, setUser] = useState<UserData | null>(null);
-  const [authData, setAuthData] = useState<AuthData | null>(null);
-  const isAuthenticated = !!user;
+	const [user, setUser] = useState<UserData | null>(null);
+	const [authData, setAuthData] = useState<AuthData | null>(null);
+	const isAuthenticated = !!user;
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const signIn = async (credentials: SignInFormData) => {
-    try {
-      const {
-        data: { token, refreshToken, roles, permissions },
-      } = await fetchData<AuthResponse>("/sessions", {
-        method: "post",
-        data: credentials,
-        type: "auth",
-      });
+	const signIn = async (credentials: SignInFormData) => {
+		try {
+			const {
+				data: { token, refreshToken, roles, permissions },
+			} = await fetchData<AuthResponse>('/sessions', {
+				method: 'post',
+				data: credentials,
+				type: 'auth',
+			});
 
-      setUser({
-        email: credentials.email,
-        roles,
-        permissions,
-      });
-      setAuthData({
-        token,
-        refreshToken,
-      });
+			// Save data on cookies
+			setCookie(undefined, 'dashgo.token', token, {
+				maxAge: 60 * 60 * 24 * 30, // 30 days
+				path: '/'
+			});
+			setCookie(undefined, 'dsahgo.refreshToken', refreshToken, {
+				maxAge: 60 * 60 * 24 * 30, // 30 days
+				path: '/',
+			});
 
-      await router.push("/dashboard");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+			setUser({
+				email: credentials.email,
+				roles,
+				permissions,
+			});
+			setAuthData({
+				token,
+				refreshToken,
+			});
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
-      {children}
-    </AuthContext.Provider>
-  );
+			await router.push('/dashboard');
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export { AuthContextProvider, AuthContext };
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,80 +1,80 @@
-import Head from "next/head";
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
-import dynamic from "next/dynamic";
-import { getChartOptions } from "shared/utils/getChartOptions";
-import { NextPageWithLayout } from "shared/@types/Layout";
-import { PanelLayout } from "components/layouts/Panel";
-import { ReactElement } from "react";
+import Head from 'next/head';
+import { Box, SimpleGrid, Text } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
+import { getChartOptions } from 'shared/utils/getChartOptions';
+import { NextPageWithLayout } from 'shared/@types/Layout';
+import { PanelLayout } from 'components/layouts/Panel';
+import { ReactElement } from 'react';
 
-const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 const CHART_SERIES = [
-  {
-    name: "series1",
-    data: [31, 120, 10, 20, 61, 18, 109],
-  },
+	{
+		name: 'series1',
+		data: [31, 120, 10, 20, 61, 18, 109],
+	},
 ];
 
 const CHART_CATEGORIES = [
-  "2021-03-18T00:00:00.000Z",
-  "2021-03-19T00:00:00.000Z",
-  "2021-03-20T00:00:00.000Z",
-  "2021-03-21T00:00:00.000Z",
-  "2021-03-22T00:00:00.000Z",
-  "2021-03-23T00:00:00.000Z",
-  "2021-03-24T00:00:00.000Z",
+	'2021-03-18T00:00:00.000Z',
+	'2021-03-19T00:00:00.000Z',
+	'2021-03-20T00:00:00.000Z',
+	'2021-03-21T00:00:00.000Z',
+	'2021-03-22T00:00:00.000Z',
+	'2021-03-23T00:00:00.000Z',
+	'2021-03-24T00:00:00.000Z',
 ];
 
 const Dashboard: NextPageWithLayout = () => {
-  return (
-    <>
-      <Head>
-        <title>Dashboard | DashGo</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Dashboard | DashGo</title>
+			</Head>
 
-      <SimpleGrid
-        flex={1}
-        minChildWidth={"320px"}
-        alignItems={"flex-start"}
-        spacing={"4"}
-      >
-        <Box
-          p={{ base: "6", md: "8" }}
-          pb={"4"}
-          bg={"gray.800"}
-          borderRadius={"8"}
-        >
-          <Text fontSize={"lg"} fontWeight={"bold"} mb={"4"}>
-            Subscribers of the week
-          </Text>
+			<SimpleGrid
+				flex={1}
+				minChildWidth={'320px'}
+				alignItems={'flex-start'}
+				spacing={'4'}
+			>
+				<Box
+					p={{ base: '6', md: '8' }}
+					pb={'4'}
+					bg={'gray.800'}
+					borderRadius={'8'}
+				>
+					<Text fontSize={'lg'} fontWeight={'bold'} mb={'4'}>
+						Subscribers of the week
+					</Text>
 
-          <Chart
-            type={"area"}
-            height={168}
-            options={getChartOptions(CHART_CATEGORIES)}
-            series={CHART_SERIES}
-          />
-        </Box>
-        <Box
-          p={{ base: "6", md: "8" }}
-          pb={"4"}
-          bg={"gray.800"}
-          borderRadius={"8"}
-        >
-          <Text fontSize={"lg"} fontWeight={"bold"} mb={"4"}>
-            Open Rate
-          </Text>
+					<Chart
+						type={'area'}
+						height={168}
+						options={getChartOptions(CHART_CATEGORIES)}
+						series={CHART_SERIES}
+					/>
+				</Box>
+				<Box
+					p={{ base: '6', md: '8' }}
+					pb={'4'}
+					bg={'gray.800'}
+					borderRadius={'8'}
+				>
+					<Text fontSize={'lg'} fontWeight={'bold'} mb={'4'}>
+						Open Rate
+					</Text>
 
-          <Chart
-            type={"area"}
-            height={168}
-            options={getChartOptions(CHART_CATEGORIES)}
-            series={CHART_SERIES}
-          />
-        </Box>
-      </SimpleGrid>
-    </>
-  );
+					<Chart
+						type={'area'}
+						height={168}
+						options={getChartOptions(CHART_CATEGORIES)}
+						series={CHART_SERIES}
+					/>
+				</Box>
+			</SimpleGrid>
+		</>
+	);
 };
 
 Dashboard.layout = (page: ReactElement) => <PanelLayout>{page}</PanelLayout>;
